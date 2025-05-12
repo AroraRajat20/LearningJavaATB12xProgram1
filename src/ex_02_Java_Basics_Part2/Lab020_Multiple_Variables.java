a309ffefc96587f23b9cb99a96995e918fb4ee85
AroraRajat20 <rajatrajat678@gmail.com>
2025-05-12 19:51:57 +0530

    java basic updates1

---
+++ src/ex_02_Java_Basics_Part2/Lab020_Multiple_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics_Part2;
+
+public class Lab020_Multiple_Variables {
+    public static void main(String[] args) {
+        int a = 10 , b =20 ,c =100;
+        System.out.println(a + " " + b+ " " + c);
+        System.out.printf("a%d,b%d,c%d",a,b,c);
+    }
+}
