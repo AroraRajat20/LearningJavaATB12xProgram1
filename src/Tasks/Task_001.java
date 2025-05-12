c6e51da48e4f46acbc5cff11b88ccd8a29f33d70
AroraRajat20 <rajatrajat678@gmail.com>
2025-05-12 21:46:52 +0530

    Java With task

---
+++ src/Tasks/Task_001.java
@@ -0,0 +1,23 @@
+package Tasks;
+
+public class Task_001 {
+    public static void main(String[] args) {
+        int num = 5;
+        char new_line = '\n';
+        System.out.printf("%d*1 = %d %c",num ,num*1,new_line);
+        System.out.printf("%d*2 = %d %c",num ,num*2,new_line);
+        System.out.printf("%d*3 = %d %c",num ,num*3,new_line);
+        System.out.printf("%d*4 = %d %c",num ,num*4,new_line);
+        System.out.printf("%d*5 = %d %c",num ,num*5,new_line);
+        System.out.printf("%d*6 = %d %c",num ,num*6,new_line);
+        System.out.printf("%d*7 = %d %c",num ,num*7,new_line);
+        System.out.printf("%d*8 = %d %c",num ,num*8,new_line);
+        System.out.printf("%d*9 = %d %c",num ,num*9,new_line);
+        System.out.printf("%d*10 = %d %c",num ,num*10,new_line);
+
+//
+//        System.out.println("5*1=" + num*1 );
+//        System.out.println("5*2=" + num*2 );
+
+    }
+}
