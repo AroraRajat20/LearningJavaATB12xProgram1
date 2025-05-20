1a94aa70bd91c0db00719ab6bccd5373790c7782
AroraRajat20 <rajatrajat678@gmail.com>
2025-05-20 20:50:48 +0530

    Java With task4

---
+++ src/Tasks/Task004.java
@@ -0,0 +1,25 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task004 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first side");
+        float firstside = scanner.nextFloat();
+        System.out.println("Enter the second side");
+        float secondside = scanner.nextFloat();
+        System.out.println("Enter the third side");
+        float thirdside = scanner.nextFloat();
+
+        if(firstside == secondside && secondside == thirdside){
+            System.out.println("equilateral triangle");}
+            else if (firstside == secondside || secondside == thirdside || thirdside == firstside) {
+            System.out.println("isosceles triangle");
+        }
+            else {
+            System.out.println("scalene triangle");
+        }
+        }
+    }
+
---
+++ src/ex_03_Literals/Lab028increment.java
@@ -0,0 +1,15 @@
+package ex_03_Literals;
+
+public class Lab028increment {
+    public static void main(String[] args) {
+        int a = 11, b = 22, c;
+
+        c = a + b + a++ + b++ + ++a + ++b;
+
+        System.out.println("a=" + a);
+
+        System.out.println("b=" + b);
+
+        System.out.println("c=" + c);
+    }
+}
