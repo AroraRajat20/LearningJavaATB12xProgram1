35983d057523d7a966b5402740101d885328e7cd
AroraRajat20 <rajatrajat678@gmail.com>
2025-05-24 21:34:51 +0530

    Java With task5

---
+++ src/Tasks/Task006Year_Problem.java
@@ -0,0 +1,35 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class Task006Year_Problem {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("enter the month number");
+        int month = scanner.nextInt();
+        System.out.println("Enter the Year");
+        int year = scanner.nextInt();
+
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                System.out.println("31 Days");
+                break;
+            case 4,6,9,11:
+                System.out.println("31 Days");
+                break;
+            case 2:
+                if(year%4==0){
+                    System.out.println("29 Days as  its Leap Year");
+                }
+                else {
+                    System.out.println("28 Days");
+                }
+                break;
+            default:
+                System.out.println("Invaild Input");
+
+
+        }
+
+    }
+}
