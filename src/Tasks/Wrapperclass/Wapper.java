410f023905fb427f9ba217e74ed023677bccb472
AroraRajat20 <rajatrajat678@gmail.com>
2025-06-27 22:14:40 +0530

    with new task

---
+++ src/Tasks/Wrapperclass/Wapper.java
@@ -0,0 +1,13 @@
+package Tasks.Wrapperclass;
+
+public class Wapper {
+    public static void main(String[] args) {
+        Integer a1 =100;
+        System.out.println(a1.byteValue());
+        System.out.println(a1.hashCode());
+        System.out.println(a1.doubleValue());
+        System.out.println(a1.compareTo(100));
+        System.out.println(a1.MAX_VALUE);
+        System.out.println(a1.MIN_VALUE);
+    }
+}
---
+++ src/Tasks/Wrapperclass/wrapperboxingandupboxing.java
@@ -0,0 +1,19 @@
+package Tasks.Wrapperclass;
+
+public class wrapperboxingandupboxing {
+    public static void main(String[] args) {
+
+
+        Integer a = 10;
+        Integer b = a; // boxing JVM will auto do this
+      //  String a1 = ;
+        System.out.println(a);
+        System.out.println(b);
+       // System.out.println(a1);
+
+        //Unboxing
+        Integer aa = 42;
+        int bb= aa;
+
+    }
+}
