3a15dcaa20b7475a93537bac99ddf73b41fc0ca1
AroraRajat20 <rajatrajat678@gmail.com>
2025-06-28 18:40:56 +0530

    with new task

---
+++ src/Tasks/Objectclass/Objectclass.java
@@ -0,0 +1,19 @@
+package Tasks.Objectclass;
+
+public class Objectclass  extends Object
+{
+    public static void main(String[] args) {
+        Objectclass obj = new Objectclass();
+
+
+    }
+    public String toString(){
+        System.out.println();
+        return "";
+    }
+    public int hashCode(){
+
+        return 0;
+    }
+
+}
